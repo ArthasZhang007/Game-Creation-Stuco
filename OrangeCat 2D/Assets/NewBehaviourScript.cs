da7e5cad0c72d56ced6b612957dd5c90aa6f11ce
haoyangcai0711 <57378377+haoyangcai0711@users.noreply.github.com>
2019-11-06 20:47:19 -0500

    first day

---
+++ OrangeCat 2D/Assets/NewBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float Speed = 2000;
+    public Vector3 LeftDir = new Vector3(-1, 0, 0);
+    public Vector3 RightDir = new Vector3(1, 0, 0);
+    //public bool localPosition = false;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 deltaLeft = Speed * LeftDir* Time.deltaTime;
+        Vector3 deltaRight = Speed * RightDir* Time.deltaTime;
+        Debug.Log(Speed);
+        Debug.Log(LeftDir);
+        Debug.Log(Time.deltaTime);
+        bool left = Input.GetKeyDown(KeyCode.LeftArrow);
+        bool right = Input.GetKeyDown(KeyCode.RightArrow);
+
+
+        if (left)
+        {
+            transform.position += deltaLeft;
+            
+        }
+        else if (right)
+        {
+            transform.position += deltaRight;
+// Debug.Log("right");
+        }
+        //else
+        //    transform.position += delta;
+    }
+}
