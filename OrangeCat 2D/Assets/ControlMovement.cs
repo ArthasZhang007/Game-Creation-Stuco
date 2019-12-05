db0a347241664326653af1bc3a889540bfde1c13
haoyangcai0711 <57378377+haoyangcai0711@users.noreply.github.com>
2019-12-04 20:22:06 -0500

    Final Game

--- OrangeCat 2D/Assets/ControlMovement.cs
+++ OrangeCat 2D/Assets/ControlMovement.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 public class ControlMovement : MonoBehaviour
 {
     // Update is called once per frame
-    public float acceleration = 2.5f;
-    public float maximumSpeed = 5;
+    public float acceleration = 7.0f;
+    public float maximumSpeed = 15.5f;
+    public GameObject background;
     void Update()
     {
         bool left = Input.GetKey(KeyCode.LeftArrow);
         bool right = Input.GetKey(KeyCode.RightArrow);
         var velocity = GetComponent<Rigidbody2D>().velocity;
+        var mass = GetComponent<Rigidbody2D>().mass;
         float speed = velocity.x;
         if (left && speed > -maximumSpeed)
         {
             //I'm going left
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(-acceleration, 0));
-        } else if (right && speed < maximumSpeed)
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-acceleration * mass, 0));
+        }
+        else if (right && speed < maximumSpeed)
         {
             //I'm going right
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(acceleration, 0));
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(acceleration * mass, 0));
         }
+        float factor = 0.12f;
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(-velocity.x * factor * mass, -velocity.y * factor * mass));
         //Debug.Log(GetComponent<Rigidbody2D>().velocity);
-        //Debug.Log(GetComponent<Rigidbody2D>().position);
+        if (GetComponent<Rigidbody2D>().position.y < -40)
+        {
+            background.SetActive(true);
+        }
     }
 }
--- OrangeCat 2D/Assets/camera.cs
+++ OrangeCat 2D/Assets/camera.cs
@@ -6,6 +6,7 @@ public class camera : MonoBehaviour
 {
     public GameObject player1;
     public GameObject player2;
+    public GameObject background;
     //public GameObject copy;
     // Start is called before the first frame update
     void Start()
@@ -20,8 +21,8 @@ public class camera : MonoBehaviour
         var pos = (player1.transform.position+ player1.transform.position)/2;
         pos.z = -10;
         pos.y = transform.position.y;
-        transform.position = pos;
-        Debug.Log(transform.position);
+        if(pos.x>=-12.0f&&pos.x<=12.0f)transform.position = pos;
+        if (background.activeInHierarchy) transform.position = new Vector3( 0 , 0 , pos.z);
     }
 }
 
